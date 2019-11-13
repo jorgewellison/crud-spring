e9e4d7f5714f36dc1fe739e83a2017a04ef5c6b4
Jorge Wellison <jorge.wellison@dcx.ufpb.br>
2019-11-13 16:44:24 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.udemy.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Categoria;
+import com.udemy.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+}
